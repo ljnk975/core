d6975989a177e4388712c774af4aa718a102cb04
Kritzel Kratzel <kritzel.kratzel@gmx.de>
2023-11-24 11:02:30 +0100

    Roll-back for src/devel/devel/src/roll/etc/bootstrap-functions.sh

--- src/devel/devel/src/roll/etc/bootstrap-functions.sh
+++ src/devel/devel/src/roll/etc/bootstrap-functions.sh
@@ -308,13 +308,13 @@ function bootstrap_init() {
 	# download binaries if necessary
 	metadata=`ls .*.metadata`
 	if [ -f "$metadata" ]; then
-		# always use supplied get_sources.sh and not that one from build host
-		src/devel/devel/bin/get_sources.sh
-#		if [ "$ROLLSROOT" ]; then
-#			$ROLLSROOT/../../bin/get_sources.sh
-#		else
-#			src/devel/devel/bin/get_sources.sh
-#		fi
+		# unset ROLLSROOT during core bootstrap for download from
+		# new rocks source repo at http://sources.rocksclusters.org
+		if [ "$ROLLSROOT" ]; then
+			$ROLLSROOT/../../bin/get_sources.sh
+		else
+			src/devel/devel/bin/get_sources.sh
+		fi
 	else
 		return 0
 	fi
