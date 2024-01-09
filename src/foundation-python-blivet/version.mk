e7f8bec101102f8b8c04ef1a283392d41149e6ce
Kritzel Kratzel <kritzel.kratzel@gmx.de>
2024-01-09 18:23:15 +0100

    Finally found a python-blivet version that works: - Tests have shown, that blivet-3.3.3 ist the _last_ working version with Python2.7 - Newer versions throw weird cyclic dependency and other errors even with simple   test cases like this one:   ---cut---   import blivet   b = blivet.Blivet()   ---cut--- - blivet failure detected during porting of kernel roll by ececuting command:   /opt/rocks/bin/python /opt/rocks/lib/python2.7/site-packages/rocks/do_partition.py

--- src/foundation-python-blivet/version.mk
+++ src/foundation-python-blivet/version.mk
@@ -1,6 +1,5 @@
 PYPKGNAME = blivet
 NAME	= foundation-python-blivet
-VERSION = 3.6.0
-#VERSION = 0.61.15.76
-RELEASE = 7
-RPM.FILES = "/opt/rocks/lib/python2*/site-packages/*\\n/opt/rocks/share/locale/*"
+VERSION = 3.3.3
+RELEASE = 0
+RPM.FILES = "/opt/rocks/lib/python2*/site-packages/*"
