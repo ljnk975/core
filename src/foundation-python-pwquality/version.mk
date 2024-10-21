30f223b9827b45fcc2190f4ebca12a48fee60d0a
Van Linh <Ljnk975@gmail.com>
2024-10-22 00:32:19 +0700

    AlmaLinux pwquality source

--- src/foundation-python-pwquality/version.mk
+++ src/foundation-python-pwquality/version.mk
@@ -1,8 +1,8 @@
 PYPKGNAME = pwquality
 NAME	= foundation-python-pwquality
 VERSION = 1.4.4
-RELEASE = 6
+RELEASE = 8
 RPM.FILES = "/opt/rocks/lib/python2*/site-packages/*"
-SRCPKGNAME = libpwquality-1.4.4-6.el8.src.rpm
-SRCPKGPATH = pub/rocky/8/BaseOS/source/tree/Packages/l
-SRCPKGHOST = https://download.rockylinux.org
+SRCPKGNAME = libpwquality-1.4.4-8.el9.src.rpm
+SRCPKGPATH = vault/9.4/BaseOS/Source/Packages
+SRCPKGHOST = https://repo.almalinux.org
