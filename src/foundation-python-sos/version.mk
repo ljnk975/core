feaad50452a22e408665c18577d76af2ce754b4c
Van Linh <Ljnk975@gmail.com>
2024-10-22 02:21:46 +0700

    Fix python sos build

--- src/foundation-python-sos/version.mk
+++ src/foundation-python-sos/version.mk
@@ -1,2 +1,6 @@
-NAME	   = foundation-python-sos
+PYPKGNAME = sos
+NAME	= foundation-python-sos
+VERSION = 3.9
+RELEASE = 5
 SRCPKGNAME = sos-3.9-5.el7.centos.11.src.rpm
+RPM.FILES = "/opt/rocks/bin/*\\n/opt/rocks/share/*\\n/opt/rocks/lib/python2*/site-packages/*"
