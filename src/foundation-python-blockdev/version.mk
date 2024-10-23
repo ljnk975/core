f3a9363f0df904206eb42487f1b31a4efc8007d6
Van Linh <Ljnk975@gmail.com>
2024-10-23 22:36:45 +0700

    Fix python blockdev build

--- src/foundation-python-blockdev/version.mk
+++ src/foundation-python-blockdev/version.mk
@@ -2,4 +2,4 @@ PYPKGNAME = blockdev
 NAME	= foundation-python-blockdev
 VERSION = 2.28
 RELEASE = 10
-RPM.FILES = "/opt/rocks/lib/*\\n/opt/rocks/share/*\\n/opt/rocks/include"
+RPM.FILES = "/opt/rocks/lib/*\\n/opt/rocks/include"
